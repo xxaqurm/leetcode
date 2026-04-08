37276f9d5549082707c8d530e0ddeb0d7f0dfa78
xxaqurm <tohanesterov003@gmail.com>
2026-04-08 20:04:48 +0700

    solve: add solution for 1 problem in Go

---
+++ task1/task_1.go
@@ -0,0 +1,19 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+		
+		if idx, ok := m[complement]; ok {
+			return []int{idx, i}
+		}
+
+		m[num] = i
+	}
+	return nil
+}
+
+func main() {
+}
